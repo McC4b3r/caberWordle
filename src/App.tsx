363d3e190e109ef14c05fa3a405806fbcc2822ac
Ryan McCabe <ryan.mccabe089@gmail.com>
2023-11-24 15:36:19 -0700

    hooked up wordlist, random word func, reset func

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import { useState } from 'react';
-import { Box, Divider, Heading } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  Heading,
+  Spinner,
+  AbsoluteCenter,
+  Center,
+} from '@chakra-ui/react';
 import { WordBoard } from './components/wordBoard';
 import { Keyboard } from './components/keyboard';
 import { useKeyboardInput } from './hooks';
@@ -7,8 +15,42 @@ import { useKeyboardInput } from './hooks';
 const App = () => {
   const [inputValues, setInputValues] = useState(Array(6).fill(""));
   const [currentRow, setCurrentRow] = useState(0);
-  const wordLength = 5;
-  console.log({ inputValues });
+  const [wordList, setWordList] = useState<string[]>([]);
+  const [targetWord, setTargetWord] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  // console.log({ inputValues });
+
+  const setRandomWord = (lib: string[]) => {
+    if (lib.length > 0) {
+      const randomIndex = Math.floor(Math.random() * lib.length);
+      setTargetWord(lib[randomIndex]);
+    }
+  }
+
+  const resetGame = () => {
+    setInputValues(Array(6).fill(""))
+    setRandomWord(wordList);
+  }
+
+  useEffect(() => {
+    const getWords = async () => {
+      try {
+        const res = await fetch('/wordlist.txt');
+        const words = await res.text();
+        const wordArray = words.split('\n');
+        setWordList(wordArray);
+        setRandomWord(wordArray);
+
+      } catch (err) {
+        console.log(err);
+      }
+      setIsLoading(false);
+    };
+
+    getWords();
+  }, []);
+
+  let length = targetWord.length;
 
   const handleInputChange = (value: string) => {
     const newValues = [...inputValues];
@@ -20,9 +62,9 @@ const App = () => {
     const currentInput = inputValues[currentRow];
     const isEnter = key === 'Enter';
     const isBackspace = key === 'Backspace';
-    const canAddChar = /^[a-zA-Z]$/.test(key) && currentInput.length < wordLength;
+    const canAddChar = /^[a-zA-Z]$/.test(key) && currentInput.length < length;
 
-    if (isEnter && currentInput.length === wordLength) {
+    if (isEnter && currentInput.length === length) {
       setCurrentRow(prevRow => (prevRow < 5 ? prevRow + 1 : prevRow));
     } else if (isBackspace && currentInput.length > 0) {
       handleInputChange(currentInput.slice(0, -1));
@@ -33,6 +75,14 @@ const App = () => {
 
   useKeyboardInput(handleKeyPress);
 
+  if (isLoading) {
+    return (
+      <AbsoluteCenter>
+        <Spinner />
+      </AbsoluteCenter>
+    );
+  }
+
   return (
     <Box>
       <Heading display="flex" justifyContent="center" my={4}>
@@ -40,18 +90,23 @@ const App = () => {
       </Heading>
       <Divider orientation='horizontal' />
       <WordBoard
-        length={wordLength}
+        length={length}
         inputValues={inputValues}
         handleInputChange={handleInputChange}
         setCurrentRow={setCurrentRow}
       />
       <Keyboard
-        length={wordLength}
+        length={length}
         inputValues={inputValues}
         currentRow={currentRow}
         handleInputChange={handleInputChange}
         setCurrentRow={setCurrentRow}
       />
+      <Center mt={8}>
+        <Button onClick={resetGame}>
+          Reset Game
+        </Button>
+      </Center>
     </Box>
   );
 }
