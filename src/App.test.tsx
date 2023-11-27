3347e6bb235b0131633d1b7a98eeff0472b40cc4
Ryan McCabe <ryan.mccabe089@gmail.com>
2023-11-27 15:54:05 -0700

    added modal tests; finished app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ describe('App', () => {
   beforeEach(() => {
     global.fetch = jest.fn(() =>
       Promise.resolve({
-        text: () => Promise.resolve('word1\nword2\nword3'),
+        text: () => Promise.resolve('apple'),
         headers: new Headers(),
         ok: true,
         redirected: false,
@@ -19,6 +19,11 @@ describe('App', () => {
     jest.restoreAllMocks();
   });
 
+  const renderAppAndWait = async () => {
+    render(<App />);
+    await waitFor(() => expect(screen.queryByTestId('loading-spinner')).not.toBeInTheDocument());
+  };
+
   it('initializes with the correct default state', () => {
     render(<App />);
     expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
@@ -27,11 +32,7 @@ describe('App', () => {
 
 
   it('renders the appropriate components after initial data fetch', async () => {
-    render(<App />);
-
-    await waitFor(() => {
-      expect(screen.queryByTestId('loading-spinner')).not.toBeInTheDocument();
-    });
+    await renderAppAndWait();
 
     expect(screen.getByTestId('word-board')).toBeInTheDocument();
     expect(screen.getByTestId('virtual-keyboard')).toBeInTheDocument();
@@ -39,29 +40,26 @@ describe('App', () => {
   });
 
   it('resets the inputValues to empty strings when "Reset Game" button is clicked', async () => {
-    const mockWord = 'apple';
+    const mockWord = 'plums';
 
-    render(
-      <App />
-    );
-
-    await waitFor(() => {
-      expect(screen.queryByTestId('loading-spinner')).not.toBeInTheDocument();
-    });
+    await renderAppAndWait();
 
     const selectElement = screen.getByTestId('select-possible-answers');
     const guessButton = screen.getByText('Guess')
+
     const optionElement = document.createElement('option');
-    optionElement.text = 'apple';
-    optionElement.value = 'apple';
+    optionElement.text = mockWord;
+    optionElement.value = mockWord;
     selectElement.appendChild(optionElement);
+
     fireEvent.change(selectElement, { target: { value: mockWord } });
     fireEvent.click(guessButton);
+
     const pinInputFields = screen.getAllByRole('textbox');
     const firstRow = pinInputFields.slice(0, 5);
 
     expect(firstRow.map((input) => (
-      input as HTMLInputElement).value)).toEqual(['a', 'p', 'p', 'l', 'e']);
+      input as HTMLInputElement).value)).toEqual(['p', 'l', 'u', 'm', 's']);
 
     const resetButton = screen.getByText('Reset Game');
     fireEvent.click(resetButton);
@@ -70,4 +68,55 @@ describe('App', () => {
       expect((input as HTMLInputElement).value).toEqual('');
     });
   });
+
+  it('handles win scenario', async () => {
+    const mockWord: string = 'apple';
+    await renderAppAndWait();
+
+    const pinInputFields: HTMLInputElement[] = screen.getAllByRole('textbox');
+    const firstRow: HTMLInputElement[] = pinInputFields.slice(0, 5);
+
+    const simulateSuccessAttempt = async () => {
+      firstRow.forEach((input, index) => fireEvent.change(input, { target: { value: mockWord[index] } }));
+
+      const expectedValue: string[] = ['a', 'p', 'p', 'l', 'e'];
+      expect(firstRow.map((input) => input.value)).toEqual(expectedValue);
+
+      fireEvent.keyDown(document.body, { key: 'Enter' });
+
+      firstRow.forEach((input) => expect(input).toHaveStyle({ backgroundColor: 'green' }));
+    };
+
+    await simulateSuccessAttempt();
+
+    expect(screen.getByText('Victory!')).toBeInTheDocument();
+    expect(screen.getByText('You correctly guessed the word: apple')).toBeInTheDocument();
+  });
+
+  it('handles loss scenario', async () => {
+    const mockWord: string = 'north';
+    await renderAppAndWait();
+
+    const pinInputFields: HTMLInputElement[] = screen.getAllByRole('textbox');
+
+    const simulateFailureAttempt = async (row: HTMLInputElement[]) => {
+      row.forEach((input, index) => fireEvent.change(input, { target: { value: mockWord[index] } }));
+
+      expect(row.map((input) => input.value)).toEqual(['n', 'o', 'r', 't', 'h']);
+
+      fireEvent.keyDown(document.body, { key: 'Enter' });
+
+      row.forEach((input) => expect(input).toHaveStyle({ backgroundColor: 'grey' }));
+    };
+
+    for (let i = 0; i < 6; i++) {
+      const row: HTMLInputElement[] = pinInputFields.slice(i * 5, (i + 1) * 5);
+      await simulateFailureAttempt(row);
+    }
+
+    expect(screen.getByText('Game Over')).toBeInTheDocument();
+    expect(screen.getByText('The word was: apple')).toBeInTheDocument();
+  });
+
+
 });
--- src/components/finishedModal/index.tsx
+++ src/components/finishedModal/index.tsx
@@ -23,6 +23,7 @@ export const FinishedModal = ({
 
   return (
     <Modal
+      data-testid="finished-modal"
       isOpen={isOpen}
       onClose={resetGame}
       isCentered
